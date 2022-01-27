0033b9223b7b8a037817159c21a0cc7ae2f1cfee
JadenFicklin <jadendficklin@gmail.com>
2022-01-27 13:19:05 -0700

    Getting ready for deployment

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 5678;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+app.use(express.json());
+app.use("/js", express.static(path.join(__dirname, "public/main.js")));
+app.use("/styles", express.static(path.join(__dirname, "../index.css")));
+app.use("/pictures", express.static(path.join(__dirname, "../pictures")));
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
